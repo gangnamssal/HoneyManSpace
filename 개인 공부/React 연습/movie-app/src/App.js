03af8b67d19d033384e8d2331b4547bc260f7454
TaeHeon <ggnsall@naver.com>
2022-12-31 00:38:53 +0900

    Movie - app part2

--- 개인 공부/React 연습/movie-app/src/App.js
+++ 개인 공부/React 연습/movie-app/src/App.js
@@ -1,45 +1,20 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5`)
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => {
-            return (
-              <div key={movie.id}>
-                <img src={movie.medium_cover_image} alt="img" />
-                <h1>{movie.title}</h1>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((g) => {
-                    return <li key={g}>{g}</li>;
-                  })}
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
